ec5ceac99820dff67d68708ba11512ffb524238d
Amine <amineninja9@gmail.com>
2024-12-19 20:02:42 +0100

    Add files via upload

---
+++ text_splitter.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+
+# Download required resources for nltk
+nltk.download("punkt")
+
+def split_text_preserve_sentences(text, max_words):
+    sentences = sent_tokenize(text)
+    chunks = []
+    current_chunk = []
+    current_word_count = 0
+
+    for sentence in sentences:
+        words_in_sentence = len(sentence.split())
+        if current_word_count + words_in_sentence > max_words:
+            chunks.append(' '.join(current_chunk))
+            current_chunk = []
+            current_word_count = 0
+        current_chunk.append(sentence)
+        current_word_count += words_in_sentence
+
+    if current_chunk:
+        chunks.append(' '.join(current_chunk))
+
+    return chunks
